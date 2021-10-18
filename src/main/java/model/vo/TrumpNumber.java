bd29f135286ef64da09d444dba9d1e3b009663cf
Byeongju-Kong <bjuuuu98@gmail.com>
2021-10-18 15:31:10 +0900

    feat : TrumpNumber 테스트 및 구현

---
+++ src/main/java/model/vo/TrumpNumber.java
@@ -0,0 +1,32 @@
+package model.vo;
+
+import java.util.Arrays;
+
+public enum TrumpNumber {
+    A(1, "A"), TWO(2, "2"), THREE(3, "3"), FOUR(4, "4"), FIVE(5, "5"),
+    SIX(6, "6"), SEVEN(7, "7"), EIGHT(8, "8"), NINE(9, "9"),
+    J(10, "J"), Q(11, "Q"), K(12, "K");
+    private final int numberIndex;
+    private final String value;
+
+    TrumpNumber(final int numberIndex, final String value) {
+        this.numberIndex = numberIndex;
+        this.value = value;
+    }
+
+    public static TrumpNumber findBy(final int numberIndex) {
+        return Arrays.stream(TrumpNumber.values())
+                .filter(trumpNumber -> trumpNumber.numberIndex == numberIndex)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Index에 대응하는 숫자가 없습니다."));
+    }
+
+    public boolean isA() {
+        return this == A;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
