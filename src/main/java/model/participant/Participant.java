29164bd4df65eb88407e767933d8582c4b7c240e
Byeongju-Kong <bjuuuu98@gmail.com>
2021-10-21 17:37:19 +0900

    feat : Participant 테스트 및 구현

---
+++ src/main/java/model/participant/Participant.java
@@ -0,0 +1,27 @@
+package model.participant;
+
+import model.BlackJackStatus;
+import model.card.Cards;
+import model.card.vo.Card;
+
+import java.util.List;
+
+public class Participant {
+    Cards cards;
+
+    public Participant(final List<Card> initialCards) {
+        this.cards = Cards.generate(initialCards);
+    }
+
+    public void draw(final Card newCards) {
+        cards.draw(newCards);
+    }
+
+    public boolean isDefeater() {
+        return cards.getStatus() == BlackJackStatus.HIGHER_THAN_21;
+    }
+
+    public boolean hasBlackJackCard() {
+        return cards.getStatus() == BlackJackStatus.BLACKJACK;
+    }
+}
---
+++ src/test/java/model/participant/ParticipantTest.java
@@ -0,0 +1,53 @@
+package model.participant;
+
+import model.card.vo.Card;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ParticipantTest {
+    @ParameterizedTest
+    @DisplayName("참가자의 숫자가 21이 넘어 패배하였는지에 대한 여부를 알려준다.")
+    @MethodSource("provideNewCardAndIsDefeater")
+    void isDefeater(Card newCard, boolean expected) {
+        List<Card> initialCards =
+                new ArrayList<>(Arrays.asList(Card.generate(4, 1), Card.generate(9, 1)));
+        Participant participant = new Participant(initialCards);
+        participant.draw(newCard);
+        boolean actual = participant.isDefeater();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideNewCardAndIsDefeater() {
+        return Stream.of(
+                Arguments.of(Card.generate(7, 1), false),
+                Arguments.of(Card.generate(9, 2), true)
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("참가자의 카드가 블랙잭인지 반환한다.")
+    @MethodSource("provideCardsAndHasBlackJackCards")
+    void hasBlackJackCards(List<Card> initialCards, boolean expected) {
+        Participant participant = new Participant(initialCards);
+        boolean actual = participant.hasBlackJackCard();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideCardsAndHasBlackJackCards() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(Card.generate(10, 1), Card.generate(11, 1)),
+                        false),
+                Arguments.of(Arrays.asList(Card.generate(1, 1), Card.generate(10, 1)),
+                        true)
+        );
+    }
+}
