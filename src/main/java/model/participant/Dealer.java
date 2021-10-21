b14f7ddd8d78949714c1160cd5b773947d98e973
Byeongju-Kong <bjuuuu98@gmail.com>
2021-10-21 16:25:10 +0900

    feat : Dealer 테스트 및 구현

---
+++ src/main/java/model/participant/Dealer.java
@@ -0,0 +1,31 @@
+package model.participant;
+
+import model.BlackJackStatus;
+import model.card.Cards;
+import model.card.vo.Card;
+
+import java.util.List;
+
+public class Dealer {
+    private final Cards cards;
+
+    public static Dealer participate(List<Card> initialCards) {
+        return new Dealer(initialCards);
+    }
+
+    private Dealer(List<Card> initialCards) {
+        this.cards = Cards.generate(initialCards);
+    }
+
+    public boolean hasCardsHigherThan16() {
+        return cards.isHigherThan16();
+    }
+
+    public void draw(Card newCards) {
+        cards.draw(newCards);
+    }
+
+    public boolean isDefeater() {
+        return cards.getStatus() == BlackJackStatus.HIGHER_THAN_21;
+    }
+}
