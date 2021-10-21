b8efcd18bd69529c8e5cc4cfcd8dd53da92ea273
Byeongju-Kong <bjuuuu98@gmail.com>
2021-10-21 15:56:06 +0900

    feat : Name 테스트 및 구현

---
+++ src/main/java/model/participant/player/Name.java
@@ -0,0 +1,35 @@
+package model.participant.player;
+
+import java.util.Objects;
+
+public class Name {
+    private final String value;
+
+    public static Name create(final String name) {
+        return new Name(name);
+    }
+
+    private Name(final String name) {
+        if(name.isBlank()) {
+            throw new IllegalArgumentException("공백으로 이름을 생성할 수 없습니다.");
+        }
+        this.value = name;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name = (Name) o;
+        return Objects.equals(value, name.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
---
+++ src/test/java/model/participant/player/NameTest.java
@@ -0,0 +1,25 @@
+package model.participant.player;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class NameTest {
+    @Test
+    @DisplayName("Blank 상태의 문자열로 객체를 생성하면 예외를 발생시킨다.")
+    void create_ExceptionByBlank() {
+        assertThatIllegalArgumentException().isThrownBy(() -> Name.create(" "))
+                .withMessage("공백으로 이름을 생성할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("이름 값을 반환한다.")
+    void value() {
+        Name name = Name.create("Brandon");
+        String actual = name.value();
+        String expected = "Brandon";
+        assertThat(actual).isEqualTo(expected);
+    }
+}
