4facf7d52fd151db230e7f7d21a54ee6e5338257
Byeongju-Kong <bjuuuu98@gmail.com>
2021-10-24 13:19:48 +0900

    feat : Sum 테스트 및 구현

--- src/main/java/model/card/Cards.java
+++ src/main/java/model/card/Cards.java
@@ -2,16 +2,14 @@ package model.card;
 
 import model.BlackJackStatus;
 import model.card.vo.Card;
-import model.card.vo.TrumpNumber;
 
 import java.util.List;
 
 import static model.BlackJackStatus.*;
 
 public class Cards {
-    private static final int VALUE_OF_JQK = 10;
     private final List<Card> values;
-    private int sumOfCardValues;
+    private Sum sumOfCardValues;
 
     public static Cards generate(List<Card> initialCards) {
         return new Cards(initialCards);
@@ -19,8 +17,7 @@ public class Cards {
 
     private Cards(List<Card> initialCards) {
         values = initialCards;
-        sumOfCardValues = 0;
-        addNumberValue();
+        sumOfCardValues = Sum.create(initialCards);
     }
 
     public List<Card> getCards() {
@@ -29,56 +26,24 @@ public class Cards {
 
     public void add(Card newCard) {
         values.add(newCard);
-        sumOfCardValues = 0;
-        addNumberValue();
+        sumOfCardValues = Sum.create(values);
     }
 
     public BlackJackStatus getStatus() {
-        if (sumOfCardValues < 21) {
-            return LOWER_THAN_21;
+        if (sumOfCardValues.value() == 21) {
+            return BLACKJACK;
         }
-        if (sumOfCardValues > 21) {
+        if (sumOfCardValues.value() > 21) {
             return BUST;
         }
-        return BLACKJACK;
+        return LOWER_THAN_21;
     }
 
     public boolean isHigherThan16() {
-        return sumOfCardValues > 16;
-    }
-
-    private void addNumberValue() {
-        values.stream()
-                .filter(card -> !card.getNumber().isA())
-                .forEach(card -> addNotA(card.getNumber()));
-        if (values.stream().anyMatch(Card::isA)) {
-            addA();
-        }
-    }
-
-    private void addNotA(TrumpNumber number) {
-        if (number.isJQK()) {
-            sumOfCardValues += VALUE_OF_JQK;
-        }
-        if (!number.isJQK()) {
-            sumOfCardValues += number.value();
-        }
-    }
-
-    private void addA() {
-        if (isSumHigherThan21IfAIsCalculatedAs11()) {
-            sumOfCardValues += 1;
-        }
-        if (!isSumHigherThan21IfAIsCalculatedAs11()) {
-            sumOfCardValues += 11;
-        }
-    }
-
-    private boolean isSumHigherThan21IfAIsCalculatedAs11() {
-        return sumOfCardValues >= 11;
+        return sumOfCardValues.value() > 16;
     }
 
     public boolean hasHigherSumOfCardValuesThan(final Cards cards) {
-        return this.sumOfCardValues >= cards.sumOfCardValues;
+        return this.sumOfCardValues.value() >= cards.sumOfCardValues.value();
     }
 }
