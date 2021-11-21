1c27635fe52299118d4dd5b282da609750165c33
Byeongju-Kong <bjuuuu98@gmail.com>
2021-11-21 16:36:58 +0900

    feat : Lose 테스트 및 구현

---
+++ src/test/java/model/state/finished/LoseTest.java
@@ -0,0 +1,24 @@
+package model.state.finished;
+
+import model.card.Cards;
+import model.card.vo.Card;
+import model.state.State;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LoseTest {
+    @Test
+    @DisplayName("수익률을 반환한다.")
+    void earningRate() {
+        Cards cards = Cards.generate(
+                Arrays.asList(Card.generate(10, 1), Card.generate(7, 2)));
+        Finished lose = new Lose(cards);
+        double actual = lose.earningRate();
+        double expected = -1.0;
+        assertThat(actual).isEqualTo(expected);
+    }
+}
