40a047a58e62488effab054ad6c21b97bb2a7a48
Byeongju-Kong <bjuuuu98@gmail.com>
2021-11-23 14:03:34 +0900

    refactor : 메소드 추가 및 삭제

--- src/main/java/model/participant/Player.java
+++ src/main/java/model/participant/Player.java
@@ -1,53 +1,37 @@
 package model.participant;
 
 import model.card.Cards;
-import model.card.vo.Card;
+import model.participant.vo.BettingMoney;
 import model.participant.vo.Name;
-import model.state.State;
 import model.state.finished.BlackJack;
 import model.state.finished.Stay;
 import model.state.running.Hit;
 
-import java.util.List;
 import java.util.Objects;
 
-public class Player {
-    protected Name name;
-    State state;
+public class Player extends Participant{
+    private final Name name;
+    private final BettingMoney bettingMoney;
 
-    public static Player participate(final String name, final Cards initialCards) {
-        return new Player(name, initialCards);
+    public static Player of(final String name, final int bettingMoney, final Cards initialCards) {
+        return new Player(name, bettingMoney, initialCards);
     }
 
-    Player(final String name, final Cards initialCards) {
+    private Player(final String name, final int bettingMoney, final Cards initialCards) {
         if (initialCards.hasSumOf21ComposedWithTwoCard()) {
             this.state = new BlackJack(initialCards);
         } else if (!initialCards.hasSumOf21ComposedWithTwoCard()) {
             this.state = new Hit(initialCards);
         }
+        this.bettingMoney = BettingMoney.from(bettingMoney);
         this.name = Name.create(name);
-    }
-
-    public void draw(final Card newCards) {
-        state = state.draw(newCards);
-    }
-
-    public Cards getCards() {
-        return state.cards();
     }
 
     public boolean hasName(final String name) {
         return this.name.value().equals(name);
     }
 
-    public boolean hasHigherCardsThan(final List<Player> others) {
-        return others.stream().allMatch(this::hasHigherCardsThan);
-    }
-
-    public boolean hasHigherCardsThan(final Player another) {
-        return state.cards().hasHigherSumOfCardValuesThan(another.state.cards());
-    }
-
+    @Override
     public boolean canDrawCards() {
         return !state.isFinished();
     }
@@ -56,15 +40,28 @@ public class Player {
         state = state.stay();
     }
 
-    public boolean hasStayState() {
-        return state instanceof Stay;
+    public void checkFinalResult(Cards dealerCards) {
+        if (state.getClass().equals(Stay.class) && getCards().hasHigherSumOfCardValuesThan(dealerCards)) {
+            state = state.win();
+        } else if (state.getClass().equals(Stay.class) && !getCards().hasHigherSumOfCardValuesThan(dealerCards)) {
+            state = state.lose();
+        }
+    }
+
+    public double getProfit() {
+        return state.profit(bettingMoney.value());
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Player that = (Player) o;
-        return Objects.equals(name, that.name);
+        Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
--- src/test/java/model/participant/PlayerTest.java
+++ src/test/java/model/participant/PlayerTest.java
@@ -11,96 +11,114 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PlayerTest {
     private final Cards initialCards = Cards.generate(
-            new ArrayList<>(Arrays.asList(Card.generate(4, 1), Card.generate(9, 1))));
-    private final Player participant = Player.participate("Brandon", initialCards);
+            new ArrayList<>(Arrays.asList(Card.generate(8, 1), Card.generate(10, 3))));
+    private final Player player = Player.of("Chris", 10000, initialCards);
 
     @ParameterizedTest
-    @DisplayName("카드를 더 draw 할 수 있는지 반환한다.")
-    @MethodSource("provideNewCardAndIsDefeater")
-    void isDefeater(Card newCard, boolean expected) {
-        participant.draw(newCard);
-        boolean actual = participant.canDrawCards();
+    @DisplayName("이름 값을 받아 자신이 갖고 있는 이름과 같은지 반환한다.")
+    @CsvSource({"Chris, true", "Brandon, false"})
+    void hasName(final String name, final boolean expected) {
+        boolean actual = player.hasName(name);
         assertThat(actual).isEqualTo(expected);
     }
 
-    private static Stream<Arguments> provideNewCardAndIsDefeater() {
-        return Stream.of(
-                Arguments.of(Card.generate(7, 1), true),
-                Arguments.of(Card.generate(9, 2), false)
-        );
-    }
-
     @Test
-    @DisplayName("카드들을 반환한다.")
+    @DisplayName("카드를 반환한다.")
     void getCards() {
-        Cards cards = participant.getCards();
-        assertThat(cards.getCards()).isEqualTo(initialCards.getCards());
-    }
-
-    @ParameterizedTest
-    @DisplayName("문자열을 받아 자신의 이름 값과 같은지 반환한다.")
-    @CsvSource({"Brandon, true", "Henry, false"})
-    void hasName(String name, boolean expected) {
-        boolean actual = participant.hasName(name);
-        assertThat(actual).isEqualTo(expected);
+        Cards actual = player.getCards();
+        assertThat(actual).isEqualTo(initialCards);
     }
 
     @Test
-    @DisplayName("Participant 객체를 받아 자신의 카드 합이 더 큰지 반환한다.")
-    void hasHigherCardsThan() {
-        Cards anotherCards = Cards.generate(
-                Arrays.asList(Card.generate(4, 2), Card.generate(8, 2)));
-        Player another = Player.participate("Henry", anotherCards);
-        assertThat(participant.hasHigherCardsThan(another)).isTrue();
-    }
-
-    @ParameterizedTest
-    @DisplayName("Player 객체들을 받아 자신의 카드 합이 가장 큰지 반환한다.")
-    @MethodSource("provideOtherPlayersAndExpected")
-    void hasHigherCardsThan(List<Player> others, boolean expected) {
-        boolean actual = participant.hasHigherCardsThan(others);
+    @DisplayName("카드를 한장 추가한다.")
+    void draw() {
+        Card newCard = Card.generate(3, 2);
+        player.draw(newCard);
+        Cards actual = player.getCards();
+        Cards expected = Cards.generate(
+                Arrays.asList(Card.generate(8, 1), Card.generate(10, 3),
+                        Card.generate(3, 2)));
         assertThat(actual).isEqualTo(expected);
     }
 
-    private static Stream<Arguments> provideOtherPlayersAndExpected() {
+    @ParameterizedTest
+    @DisplayName("카드를 더 뽑을 수 있는지 반환한다.")
+    @MethodSource("provideInitialCardsAndCanDrawCards")
+    void canDrawCards(final Cards initialCards, final boolean expected) {
+        Player player = Player.of("Chris", 10000, initialCards);
+        boolean actual = player.canDrawCards();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideInitialCardsAndCanDrawCards() {
         return Stream.of(
-                Arguments.of(Arrays.asList(
-                        Player.participate("Brandon", Cards.generate(Arrays.asList(
-                                Card.generate(5, 2), Card.generate(8, 4)))),
-                        Player.participate("Brandon", Cards.generate(Arrays.asList(
-                                Card.generate(4, 2), Card.generate(7, 4))))), true),
-                Arguments.of(Arrays.asList(
-                        Player.participate("Brandon", Cards.generate(Arrays.asList(
-                                Card.generate(5, 2), Card.generate(1, 4)))),
-                        Player.participate("Brandon", Cards.generate(Arrays.asList(
-                                Card.generate(5, 2), Card.generate(8, 4))))), false)
+                Arguments.of(Cards.generate(Arrays.asList(
+                        Card.generate(1, 2), Card.generate(9, 2))), true),
+                Arguments.of(Cards.generate(Arrays.asList(
+                        Card.generate(1, 2), Card.generate(10, 2))), false)
         );
     }
 
     @Test
-    @DisplayName("상태를 stay로 변경한다.")
+    @DisplayName("상태를 Stay로 변경한다.")
     void stay() {
-        participant.stay();
-        boolean actual = participant.hasStayState();
-        assertThat(actual).isTrue();
+        player.stay();
+        boolean ableToDraw = player.canDrawCards();
+        assertThat(ableToDraw).isFalse();
     }
 
-    @Test
-    @DisplayName("이름을 기준으로 같은 참가자인지 반환한다.")
-    void equals() {
-        Cards firstCards = Cards.generate(
-                Arrays.asList(Card.generate(1, 1), Card.generate(10, 1)));
-        Cards secondCards = Cards.generate(
-                Arrays.asList(Card.generate(3, 1), Card.generate(10, 1)));
-        Player first = Player.participate("Brandon", firstCards);
-        Player second = Player.participate("Brandon", secondCards);
-        assertThat(first.equals(second)).isTrue();
+    @ParameterizedTest
+    @DisplayName("수익을 반환한다.")
+    @MethodSource("provideDealerCardsAndExpectedProfit")
+    void getProfit(final Cards dealerCards, double expectedProfit) {
+        player.stay();
+        player.checkFinalResult(dealerCards);
+        double actualProfit = player.getProfit();
+        assertThat(actualProfit).isEqualTo(expectedProfit);
+    }
+
+    private static Stream<Arguments> provideDealerCardsAndExpectedProfit() {
+        return Stream.of(
+                Arguments.of(Cards.generate(Arrays.asList(
+                        Card.generate(8, 1), Card.generate(9, 4))), 10000.0),
+                Arguments.of(Cards.generate(Arrays.asList(
+                        Card.generate(10, 1), Card.generate(9, 4))), -10000.0)
+        );
+    }
+
+    private Cards anotherCards = Cards.generate(
+            Arrays.asList(Card.generate(1, 3), Card.generate(7, 3)));
+
+    @ParameterizedTest
+    @DisplayName("이름을 기준으로 같은 객체인지 반환한다.")
+    @MethodSource("provideNamesAndExpectationOfEqualsAndHashCode")
+    void equals(final String name, final boolean expected) {
+        Player another = Player.of(name, 20000, anotherCards);
+        boolean actual = player.equals(another);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("이름을 기준으로 같은 해쉬코드를 반환한다.")
+    @MethodSource("provideNamesAndExpectationOfEqualsAndHashCode")
+    void hashCode(final String name, final boolean expected) {
+        Player anotherPlayer = Player.of(name, 20000, anotherCards);
+        int playerHashCode = player.hashCode();
+        int anotherPlayerHashCode = anotherPlayer.hashCode();
+        boolean actual = playerHashCode == anotherPlayerHashCode;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideNamesAndExpectationOfEqualsAndHashCode() {
+        return Stream.of(
+                Arguments.of("Chris", true),
+                Arguments.of("Brandon", false)
+        );
     }
 }
